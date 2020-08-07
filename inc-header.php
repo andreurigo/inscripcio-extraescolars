c5eb76b1cb7a5ec1a0535fe6cde293433021ee2d
Andreu Rigo <andreu.rigo.gost@gmail.com>
2020-08-07 06:20:23 +0000

    adding css

--- inc-header.php
+++ inc-header.php
@@ -18,18 +18,20 @@ require('connect.php');
 list($year,$month,$day,$hour,$minute)=explode("-",$conf['activacio']);
 if ($conf['debug']=='on') echo "year: $year month: $month day: $day hour: $hour minute: $minute <br />\n";
 if ($conf['debug']=='on') echo "time: ".time()."<br />\n"; //Actual
+$correcciohoraservidor=(int)$conf['correcciohoraservidor'];
+if ($conf['debug']=='on') echo "time amb correccio: ".(time()+$correcciohoraservidor)."<br />\n"; //Actual corregit
 if ($conf['debug']=='on') echo "activacio: ".mktime($hour,$minute,0,$month,$day,$year)."<br />\n"; //Activació de la configuració
-if (time()>mktime($hour,$minute,0,$month,$day,$year)) {
+if ((time()+$correcciohoraservidor)>mktime($hour,$minute,0,$month,$day,$year)) {
   $_SESSION['activat']=TRUE;
 } else {
   $_SESSION['activat']=FALSE;
+  $faltenminuts=(int)((mktime($hour,$minute,0,$month,$day,$year)-(time()+$correcciohoraservidor))/60);
 }
 // Prints de debug
 if ($conf['debug']=='on') echo '$conf<br>'.nl2br(print_r($conf,TRUE))."<br>";
 //if ($conf['debug']=='on') echo "<br>";
 //if ($conf['debug']=='on'&& isset($_SESSION)) print_r($_SESSION);
 if ($conf['debug']=='on'&& isset($_SESSION)) echo '$_SESSION<br>'.nl2br(print_r($_SESSION,TRUE))."<br>";
-$title="Inscripció Activitats Extraescolars {$conf['nomcentre']} {$conf['cursactual']}";
 // Per protecció de dades, esborram el fitxer d'exportació si existeix
 if(file_exists('output/inscripcions.csv')) {
  unlink ('output/inscripcions.csv');
