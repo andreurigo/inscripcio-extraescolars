461e129d7e1eba00f0c64c245fa19a5a7ed46bde
Andreu Rigo <andreu.rigo.gost@gmail.com>
2020-08-07 06:18:33 +0000

    adding css

--- index.php
+++ index.php
@@ -1,29 +1,23 @@
 <?php
 require('inc-header.php');
+$title="Inscripció Activitats Extraescolars<br />{$conf['nomcentre']}<br />{$conf['cursactual']}";
 define('TITLE',$title);
+
+
+require('inc-html-head.php');
+
+htmltitle(TITLE);
+
 ?>
-<!doctype html>
 
-<html lang="en">
-<head>
-  <meta charset="utf-8">
 
-  <title><?php echo TITLE ?></title>
-  <meta name="description" content="<?php echo TITLE ?>">
-  <meta name="author" content="Andreu Rigo">
-
-  <link rel="stylesheet" href="css/styles.css?v=1.0">
-
-</head>
-
-<body>
-  <!-- <script src="js/scripts.js"></script> -->
-  <h1><?php echo TITLE ?></h1>
 <?php
   //require("connect.php");
   //require("functions.php");
 if ($_SERVER['REQUEST_METHOD']=='POST'){
 
+if (!empty($_POST['nom']) && !empty($_POST['correu'])) {
+  
 $to = $_POST['correu'];
 $subject = "Codi Inscripció";
 $codi=rand(10000,99999);
@@ -35,31 +29,42 @@ mail($to, $subject, $body);
   $_SESSION['codi']=$codi;
   
   redirect_user("validaciocodi.php");
+
+} else {
+  echo "<h2 class='error'>Ni el nom ni el correu es poden deixar en blanc.</h2><p>Espitgi el botó per recarregar la pàgina.</p>";
+  htmlbuttonleftlink('Recarrega',$filename);  
+}
+  
   
 } else {
   
   
 ?>
 
-  <p>
+  <h2>
     <?php
       if ($_SESSION['activat']) {
         echo "El procés d'inscripció està actualment obert";
       } else {
         echo "La inscripció s'activarà a les $hour:$minute el $day del $month de $year";
+        if ($faltenminuts<600) echo "<br />Falten $faltenminuts minuts";
       }
     ?>
-  </p>
+  </h2>
   
-<form method="POST" action="">
-  <label>Nom: <input type="text" name="nom"></label><br />
-  <label>Correu: <input type="email" name="correu"></label><br />
-  <input type="submit">
+<form method="POST" action="<?php echo $filename;?>">
+<!--   <label>Nom: <input type="text" name="nom"></label><br /> -->
+  <?php htmlinputtext('nom','Introdueixi un nom','Nom i llinatges de qui fa la inscripció','Escrigui aquí el seu nom');?>
+  <?php htmlinputtext('correu','Introdueixi un correu vàlid','Correu electrònic de qui fa la inscripció','Escrigui aquí el seu email');?>
+<!--   <label>Correu: <input type="email" name="correu"></label><br /> -->
+  <?php htmlbuttonsubmit('Enviar'); ?>
 </form>
+
+	<script src="js/main.js"></script>
 
 <?php
 }
+
+require('inc-html-foot.php');
 ?>
   
-</body>
-</html>
